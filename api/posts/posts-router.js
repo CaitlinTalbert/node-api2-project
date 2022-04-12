1a621ccb06c78f283654c20d4aad79c045d5c6cb
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-12 15:31:41 -0500

    post working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -32,4 +32,28 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  try {
+    if (!contents || !title) {
+      res.status(400).json({
+        message: "Please provide title and contents for the post",
+      });
+    } else {
+      Posts.insert({ title, contents })
+        .then(({ id }) => {
+          return Posts.findById(id);
+        })
+        .then((post) => {
+          res.status(201).json(post);
+        });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the post to the database",
+      error: err.message,
+    });
+  }
+});
+
 module.exports = router;
