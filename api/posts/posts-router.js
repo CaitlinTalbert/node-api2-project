9522948d8e844630e9f076e6f690b847786d3771
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-12 15:20:12 -0500

    get with id working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,4 +14,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      res.json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be retrieved",
+      error: err.message,
+    });
+  }
+});
+
 module.exports = router;
