e857965527720349ab937386922ad34616240a6c
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-12 15:15:56 -0500

    posts working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const Posts = require("./posts-model");
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: "The posts information could not be retrieved",
+      error: err.message,
+    });
+  }
+});
+
+module.exports = router;
